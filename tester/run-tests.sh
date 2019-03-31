7675f1dd6008963af1aee5ec277817f6aa745505
Remzi Arpaci-Dusseau <remzi.arpacidusseau@gmail.com>
2019-03-31 09:06:54 -0500

    a few nits with script with color and formatting of output

--- tester/run-tests.sh
+++ tester/run-tests.sh
@@ -183,12 +183,13 @@ fi
 # do a one-time setup step
 if (( $skippre == 0 )); then
     if [[ -f tests/pre ]]; then
-	builtin echo -e "\e[32mdoing one-time pre-test\e[0m (use -s to suppress)"
+	builtin echo -e "\e[33mdoing one-time pre-test\e[0m (use -s to suppress)"
 	source tests/pre
 	if (( $? != 0 )); then
 	    echo "pre-test: failed"
 	    exit 1
 	fi
+	echo ""
     fi
 fi
 
