05d9dd2cb8cbec428fe57361e5e5d75e6346cba6
Remzi Arpaci-Dusseau <remzi.arpacidusseau@gmail.com>
2019-03-31 08:57:03 -0500

    better message for testing wrapper

--- initial-xv6/test-getreadcount.sh
+++ initial-xv6/test-getreadcount.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 if ! [[ -d src ]]; then
-    echo "src dir does not exist"
+    echo "The src/ dir does not exist."
+    echo "Your xv6 code should be in the src/ directory"
+    echo "to enable the automatic tester to work."
     exit 1
 fi
 
