21f93290c735724f6f84416a6f391663bc51ac57
Ben Wallace <bencwallace@gmail.com>
2022-03-13 18:34:31 -0400

    start work on wish shell
    This commit implements basic command parsing and handles the exit command
    for the wish shell in both interactive and non-interactive modes.

---
+++ processes-shell/wish.c
@@ -0,0 +1,55 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char sep[2] = " \n";
+char error_message[30] = "An error has occurred\n";
+
+
+void print_error_and_exit() {
+    fprintf(stderr, error_message, strlen(error_message));
+    exit(1);
+}
+
+
+void run_exit(char *args) {
+    if (args != NULL && *args != '\0') print_error_and_exit();
+    exit(0);
+}
+
+
+int main(int argc, char *argv[]) {
+    bool interactive = true;
+    FILE *fin;
+    if (argc > 2) {
+        fprintf(stderr, "too many arguments\n");
+        return 1;
+    } else if (argc == 2) {
+        interactive = false;
+        fin = fopen(argv[1], "r");
+        if (fin == NULL) {
+            fprintf(stderr, "file not found\n");
+            return 1;
+        }
+    } else fin = stdin;
+
+    // TODO: initialize PATH=/bin
+
+    char *line = NULL;
+    size_t len;
+    int num_lines = -1;
+    do {
+        if (interactive) printf("wish> ");
+        num_lines = getline(&line, &len, fin);
+        // TODO:
+        // * execute built-in commands: cd (1 arg), path (any args, overwrites path)
+        // * look up executables in path with access
+        // * execute commands with fork, execv, wait/waitpid
+        // * implement redirection, parallel commands, etc.
+        char *command = strsep(&line, sep);
+        if (strcmp(command, "exit") == 0) run_exit(line);
+        else print_error_and_exit();
+    } while (num_lines != -1);
+}
