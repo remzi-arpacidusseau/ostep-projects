b0d0fdb3f32e12d1c72243330805b2e44bf80983
Ben Wallace <bencwallace@gmail.com>
2022-03-13 21:13:45 -0400

    implement exec for wish

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -4,6 +4,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+
+
+typedef struct StringList {
+    int size;
+    char **strs;
+} StringList;
 
 
 char sep[2] = " \n";
@@ -13,6 +20,33 @@ char error_message[30] = "An error has occurred\n";
 void print_error_and_exit() {
     fprintf(stderr, error_message, strlen(error_message));
     exit(1);
+}
+
+
+StringList *parse_args(char *buffer) {
+    StringList *args = malloc(sizeof(StringList));
+    args->size = 0;
+    args->strs = NULL;
+    if (buffer == NULL) return args;
+
+    args->size = 1;
+    args->strs = malloc(sizeof(char *));
+    if (args->strs == NULL) print_error_and_exit();
+
+    int i = 0;
+    while (buffer != NULL && *buffer != '\0') {
+        char *arg = strsep(&buffer, sep);
+        if (i >=  args->size) {
+            args->size *= 2;
+            args->strs = realloc(args->strs, args->size * sizeof(char *));
+            if (args->strs == NULL) print_error_and_exit();
+        }
+        args->strs[i] = malloc(strlen(arg));
+        if (args->strs[i] == NULL) print_error_and_exit();
+        strcpy(args->strs[i], arg);
+        ++i;
+    }
+    return args;
 }
 
 
@@ -29,31 +63,41 @@ void run_cd(char *args) {
 }
 
 
-int run_path(char *buffer, char **paths) {
-    int size = 1;
-    paths = malloc(size * sizeof(char **));
-    if (paths == NULL) print_error_and_exit();
+StringList *run_path(char *buffer) {
+    return parse_args(buffer);
+}
 
-    int i = 0;
-    while (buffer != NULL && *buffer != '\0') {
-        char *path = strsep(&buffer, " ");
-        if (i >=  size) {
-            size = 2 * size;
-            paths = realloc(paths, size * sizeof(char **));
-            if (paths == NULL) print_error_and_exit();
+
+void run_exec(char *cmd, char *args, int num_paths, StringList *paths) {
+    for (int i = 0; i < num_paths; ++i) {
+        char *path = malloc(strlen(paths->strs[i]) + 1 + strlen(cmd));
+        if (path == NULL) print_error_and_exit();
+        path = strcpy(path, paths->strs[i]);
+        strcat(path, "/");
+        strcat(path, cmd);
+        if (access(path, X_OK) == 0) {
+            pid_t pid = fork();
+            if (pid == 0) {  // child
+                StringList *parsed_args = parse_args(args);
+                char **cmd_args = malloc((2 + parsed_args->size) * sizeof(char *));
+                if (cmd_args == NULL) print_error_and_exit();
+                cmd_args[0] = cmd;
+                for (int i = 1; i < 1 + parsed_args->size; ++i) cmd_args[i] = parsed_args->strs[i - 1];
+                cmd_args[1 + parsed_args->size] = NULL;
+                execv(path, cmd_args);
+            } else {  // parent
+                waitpid(pid, NULL, 0);
+            }
+            return;
         }
-        paths[i] = malloc(strlen(path));
-        strcpy(paths[i], path);
-        ++i;
     }
-    return i;
+    print_error_and_exit();
 }
 
 
 int main(int argc, char *argv[]) {
-    char **paths = NULL;
-    int num_paths = run_path("/bin", paths);
-    if (num_paths != 1) print_error_and_exit();
+    StringList *paths = run_path("/bin");
+    if (paths->size != 1) print_error_and_exit();
 
     bool interactive = true;
     FILE *fin;
@@ -71,18 +115,17 @@ int main(int argc, char *argv[]) {
 
     char *line = NULL;
     size_t len;
-    int num_lines = -1;
-    do {
-        if (interactive) printf("wish> ");
-        num_lines = getline(&line, &len, fin);
+    if (interactive) printf("wish> ");
+    while (getline(&line, &len, fin) != -1) {
         // TODO:
         // * look up executables in path with access
         // * execute commands with fork, execv, wait/waitpid
         // * implement redirection, parallel commands, etc.
-        char *command = strsep(&line, sep);
-        if (strcmp(command, "exit") == 0) run_exit(line);
-        else if (strcmp(command, "cd") == 0) run_cd(line);
-        else if (strcmp(command, "path") == 0) num_paths = run_path(line, paths);
-        else print_error_and_exit();
-    } while (num_lines != -1);
+        char *cmd = strsep(&line, sep);
+        if (strcmp(cmd, "exit") == 0) run_exit(line);
+        else if (strcmp(cmd, "cd") == 0) run_cd(line);
+        else if (strcmp(cmd, "path") == 0) paths = run_path(line);
+        else run_exec(cmd, line, paths->size, paths);
+        if (interactive) printf("wish> ");
+    }
 }
